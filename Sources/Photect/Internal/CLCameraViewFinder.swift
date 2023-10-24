d61915f58ce13b2e14247c4d469b0088f6822f12
Nicklas Jensen <800751+Nillerr@users.noreply.github.com>
2023-10-24 19:55:53 +0200

    resizeAspectFill

--- Sources/Photect/Internal/CLCameraViewFinder.swift
+++ Sources/Photect/Internal/CLCameraViewFinder.swift
@@ -160,7 +160,7 @@ internal class CLCameraViewFinder: UIView, AVCaptureVideoDataOutputSampleBufferD
     }
     
     private func showCameraFeed() {
-        self.previewLayer.videoGravity = .resizeAspect
+        self.previewLayer.videoGravity = .resizeAspectFill
         self.layer.addSublayer(self.previewLayer)
         self.previewLayer.frame = self.layer.frame
     }
